500446afb34f77161af79f0efbf6090a1162d71c
shankhanil007 <shankhanilborthakur@gmail.com>
2021-07-01 18:28:49 +0530

    Component setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,16 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Zoom from "./components/Zoom";
+import Join from "./components/Join/Join";
 
 import "./App.css";
 
 const App = () => {
   return (
-    <>
-      <Router>
-        <Route path="/" exact component={Zoom} />
-      </Router>
-    </>
+    <Router>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Zoom} />
+    </Router>
   );
 };
 
--- client/src/components/Zoom.js
+++ client/src/components/Zoom.js
@@ -1,4 +1,9 @@
 import React from "react";
+import io from "socket.io-client";
+
+const ENDPOINT = "localhost:5000";
+
+let socket;
 
 function Zoom() {
   return <></>;
