c7fafc37aad4e8e1e605047d9eb401631242a95f
shankhanil007 <shankhanilborthakur@gmail.com>
2021-07-01 18:12:56 +0530

    Socket IO setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React, { Fragment } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Zoom from "./components/Zoom";
 
 import "./App.css";
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Route path="/" exact component={Zoom} />
+      </Router>
+    </>
+  );
 };
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,23 @@
-const io = require("socket.io")(3000);
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const cors = require("cors");
 
-const users = {};
+const router = require("./router");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(cors());
+app.use(router);
+
+const obj = [
+  {
+    question: "What is Harry Potter",
+    answer: "Tom Riddle",
+  },
+];
 
 io.on("connection", (socket) => {
   console.log("user connected");
